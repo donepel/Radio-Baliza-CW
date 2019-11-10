7c1375701e277dc317cc3686f95b9310b63b738a
donepel <epel@epelbyte.com>
2019-11-10 18:38:20 -0300

    agrego funcion de codificacion de texto a CW, armo el main, y dejo casi listo.

--- Radio-Baliza-CW.ino
+++ Radio-Baliza-CW.ino
@@ -1,14 +1,19 @@
-/*********************************************************
- * Radiobaliza CW que envia tonos de audio armada con Arduino Mega 
+/******************************************************************
+ * Radiobaliza CW que envia tonos de audio para ser modulados en FM. 
+ * Diseñado para ser armada con Arduino 
  * by @Don_Epel
- * 
+ * ,
  * Fecha 10/11/2019
  * Version 0.1
- *********************************************************/
- 
+ *****************************************************************/
+
+/////////////////////////////////// DEFINICIONES //////////////////////////////////// 
  #define AUDIO  11 //PIN 11 como salida de audio
  #define PTT    12 //PIN 12 utilizado para pulsar el PTT de la radio
  #define FREC   700 //Defino la frecuencia FREC en herts y su correspondiente valor en 700 para el tono del CW
+ #define PIN_OUT 13 // pin out prueba de led
+ #define UNIT_LENGTH 150 //duracion de cada punto, las rayas duran 3 puntos
+///////////////////////////////////// SETUP ////////////////////////////////////////
 
 void setup() 
 {
@@ -18,13 +23,48 @@ void setup()
   digitalWrite(AUDIO,LOW); //establezco AUDIO en 0 para su nivel inicial
   digitalWrite(PTT,LOW); //establezco PTT en 0 para su nivel inicial
   Serial.begin(9600); //inicializo el puerto serie para debug
+  pinMode(PIN_OUT,OUTPUT);
+  digitalWrite(PIN_OUT,LOW);
 }
 
+///////////////////////////////////// MAIN /////////////////////////////////////////////
 void loop() 
 {
-  // put your main code here, to run repeatedly:
-Oscilador (AUDIO,FREC);
+  
+String textomorse = codificar( "CQ CQ CQ" );
+  for(int i=0; i<=textomorse.length(); i++)
+  {
+    switch( textomorse[i] )
+    {
+      case '.': //punto
+        for (unsigned int n=0; n<UNIT_LENGTH*2; n++)
+        {
+          Oscilador (AUDIO,FREC);
+          
+        }
+        delay( UNIT_LENGTH );
+          
+        break;
+
+      case '-': //raya
+        for (unsigned int n=0; n<=UNIT_LENGTH*6; n++)
+        {
+          Oscilador (AUDIO,FREC);
+        }
+        delay( UNIT_LENGTH );
+          
+        break;
+
+      case ' ': //gap
+        delay( UNIT_LENGTH );
+    }
+  }
+  delay (1000);
 }
+
+
+
+/////////////////////////////////// FUNCIONES //////////////////////////////////////////
 
 //Funcion de oscilador
 //Recibe: El puerto a oscilar y la frecuencia
@@ -39,4 +79,85 @@ void Oscilador (unsigned char salida,unsigned int frecuencia)
     delayMicroseconds(periodo/2);
     digitalWrite(salida,LOW);
     delayMicroseconds(periodo/2);
+}
+
+//Tabla de Morse
+static const struct {const char letra, *codigo;} TablaMorse[] =
+{
+  //Letras
+  { 'A', ".-" },
+  { 'B', "-..." },
+  { 'C', "-.-." },
+  { 'D', "-.." },
+  { 'E', "." },
+  { 'F', "..-." },
+  { 'G', "--." },
+  { 'H', "...." },
+  { 'I', ".." },
+  { 'J', ".---" },
+  { 'K', "-.-" },
+  { 'L', ".-.." },
+  { 'M', "--" },
+  { 'N', "-." },
+  { 'O', "---" },
+  { 'P', ".--." },
+  { 'Q', "--.-" },
+  { 'R', ".-." },
+  { 'S', "..." },
+  { 'T', "-" },
+  { 'U', "..-" },
+  { 'V', "...-" },
+  { 'W', ".--" },
+  { 'X', "-..-" },
+  { 'Y', "-.--" },
+  { 'Z', "--.." },
+  { ' ', "     " },//Espacio entre palabras, 7 unidades    
+  //Numeros
+  { '1', ".----" },
+  { '2', "..---" },
+  { '3', "...--" },
+  { '4', "....-" },
+  { '5', "....." },
+  { '6', "-...." },
+  { '7', "--..." },
+  { '8', "---.." },
+  { '9', "----." },
+  { '0', "-----" },
+  //Caracteres especiales  
+  { '.', "·–·–·–" },
+  { ',', "--..--" },
+  { '?', "..--.." },
+  { '!', "-.-.--" },
+  { ':', "---..." },
+  { ';', "-.-.-." },
+  { '(', "-.--." },
+  { ')', "-.--.-" },
+  { '"', ".-..-." },
+  { '@', ".--.-." },
+  { '&', ".-..." },
+};
+
+//Funcion de codificar
+//Recibe: string de texto para codificar a CW
+//Retorna: string de texto codificado en CW
+//Ejemplo codificar("CQ CQ CQ ");
+String codificar(const char *string)
+{
+  size_t i, j;
+  String textomorse = "";
+  
+  for( i = 0; string[i]; ++i )
+  {
+    for( j = 0; j < sizeof TablaMorse / sizeof *TablaMorse; ++j )
+    {
+      if( toupper(string[i]) == TablaMorse[j].letra )
+      {
+        textomorse += TablaMorse[j].codigo;
+        break;
+      }
+    }
+    textomorse += " "; //Agrego un espacio adicional para separar los caracteres
+   }
+
+  return textomorse;  
 }
