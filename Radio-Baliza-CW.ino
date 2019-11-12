79d8ad9b7ea2322968ec90812be244f9743c40fa
donepel <epel@epelbyte.com>
2019-11-12 20:41:20 -0300

    se genera funcion de destello para el led de pausa, se agrega un led de tx, se termina de pulir codigo

--- Radio-Baliza-CW.ino
+++ Radio-Baliza-CW.ino
@@ -3,13 +3,14 @@
  * Diseñado para ser armada con Arduino 
  * by @Don_Epel
  * ,
- * Fecha 10/11/2019
- * Version 0.1
+ * Fecha 12/11/2019
+ * 
  *****************************************************************/
 
 /////////////////////////////////// DEFINICIONES //////////////////////////////////// 
  //Definiciones de puertos
- #define LED_PAUSA    9  //PIN 09 como salida luminica testigo de pausa o estado de reposo
+ #define LED_TX      8   //PIN 08 como salida luminica testico de estado de TX
+ #define LED_PAUSA    9   //PIN 09 como salida luminica testigo de pausa o estado de reposo
  #define LED_CW       10  //PIN 10 como salida luminica en CW testigo del audio emitido
  #define AUDIO        11  //PIN 11 como salida de audio
  #define PTT          12  //PIN 12 utilizado para pulsar el PTT de la radio
@@ -38,80 +39,78 @@ void setup()
   pinMode(LED_CW,OUTPUT); //seteo LED_CW como SALIDA
   digitalWrite(LED_CW,LOW); //establezco LED_CW en 0 para su nivel inicial
   pinMode(LED_PAUSA,OUTPUT); //seteo LED_PAUSA como SALIDA
-  digitalWrite(LED_PAUSA,LOW); //establezco LED_PAUSA en 0 para su nivel inicial  
+  digitalWrite(LED_PAUSA,LOW); //establezco LED_PAUSA en 0 para su nivel inicial 
+  pinMode(LED_TX,OUTPUT); //seteo LED_PTT como SALIDA
+  digitalWrite(LED_TX,LOW); //establezco LED_PTT en 0 para su nivel inicial
+   
 }
 
 ///////////////////////////////////// MAIN /////////////////////////////////////////////
 void loop() 
 {
  unsigned char estado=TRANSMISION;
- //estado=TRANSMISION;
- 
  while(1)
  {
     switch(estado)
     {
       case TRANSMISION:
-                //Caso de transmision
-                  digitalWrite(PTT,HIGH); //pulso el PTT de la radio
-                  delay(500); // mantengo una pausa por si en el equipo transmisor existe alguna latencia interna
-                              // para no perder los primeros caracteres que se transmiten
-                    //Genero los tonos de cw audibles
-                   String textomorse = codificar("HOLA MUNDO ");
-                    for(int i=0; i<=textomorse.length(); i++)
-                    {
-                      switch( textomorse[i] )
-                      {
-                        case '.': //punto
-                          for (unsigned int n=0; n<=DURACIONPTO; n++)
-                          {
-                            digitalWrite(LED_CW,HIGH); //Enciendo LED_CW CW
-                            Oscilador (AUDIO,FREC);
-                            digitalWrite(LED_CW,LOW); //Apago LED_CW CW
+                        //Caso de transmision
+                        digitalWrite(PTT,HIGH); //pulso el PTT de la radio
+                        digitalWrite(LED_TX,HIGH);//Enciendo testigo TX
+                        delay(500); // mantengo una pausa por si en el equipo transmisor existe alguna latencia interna
+                                    // para no perder los primeros caracteres que se transmiten
+                        //Genero los tonos de cw audibles
+                        String textomorse = codificar("HOLA MUNDO ");
+                        for(int i=0; i<=textomorse.length(); i++)
+                        {
+                          switch( textomorse[i] )
+                            {
+                              case '.': //punto
+                                        for (unsigned int n=0; n<=DURACIONPTO; n++)
+                                        {
+                                          digitalWrite(LED_CW,HIGH); //Enciendo LED_CW CW
+                                          Oscilador (AUDIO,FREC);
+                                          digitalWrite(LED_CW,LOW); //Apago LED_CW CW
+                                        }
+                                        delay(DURACIONPTO);
+                                        break;
+                          
+                              case '-': //raya
+                                        for (unsigned int n=0; n<=DURACIONPTO*3; n++)
+                                        {   
+                                          digitalWrite(LED_CW,HIGH); //Enciendo LED_CW CW
+                                          Oscilador (AUDIO,FREC);
+                                          digitalWrite(LED_CW,LOW); //Enciendo LED_CW CW
+                                         }
+                                         delay(DURACIONPTO);
+                                         break;
+                                
+                              case ' ': //espacio
+                                        delay(DURACIONPTO);
+                                        break;
+                            }
                           }
-                          delay(DURACIONPTO);
-                          break;
-                  
-                        case '-': //raya
-                          for (unsigned int n=0; n<=DURACIONPTO*3; n++)
-                           {   
-                            digitalWrite(LED_CW,HIGH); //Enciendo LED_CW CW
-                            Oscilador (AUDIO,FREC);
-                            digitalWrite(LED_CW,LOW); //Enciendo LED_CW CW
-                           }
-                           delay(DURACIONPTO);
-                           break;
-                        
-                        case ' ': //espacio
-                          delay(DURACIONPTO);
-                          break;
-                      }
-                    }
-                    estado=REPOSO; //Finalizada la transmision, paso a la etapa de reposo.
-                    delay(500); //mantengo una pausa por si en el equipo transmisor existe alguna latencia interna
-                    digitalWrite(PTT,LOW); //suelto el PTT de la radio
-                    
-                                        
+                          estado=REPOSO; //Finalizada la transmision, paso a la etapa de reposo.
+                          delay(500); //mantengo una pausa por si en el equipo transmisor existe alguna latencia interna
+                          digitalWrite(PTT,LOW); //suelto el PTT de la radio
+                          digitalWrite(LED_TX,LOW);//apago testigo TX
     case REPOSO:
                 //Caso de reposo, espo durante la pausa 
                digitalWrite(LED_PAUSA,HIGH); //Aviso mediante testigo que me encuentro en periodo de pausa
-               delay(1000*5*PAUSA); //demoras de 1 minuto x tiempo de PAUSA definido en seccion definiciones
-               digitalWrite(LED_PAUSA,LOW); //finalizada la pausa, apago el testigo
+               for(unsigned int n=0;n<=60*PAUSA ;n++) //la pausa da la cantidad de minutos
+                  destello(LED_PAUSA);//delay de 1 segundo
                estado=TRANSMISION; //Finalizada la etapa re reposo, regreso a transmision.
                break;
     }
  }
-  
 }
-
-
 
 /////////////////////////////////// FUNCIONES //////////////////////////////////////////
 
 //Funcion de oscilador
+//Descripcion: Se genera una señal cuadrada por el pin PUERTO de una frecuencia dada (FREC)
 //Recibe: El puerto a oscilar y la frecuencia
 //Retorna: Nada
-//Nota: Se genera una señal cuadrada por el pin PUERTO de una frecuencia dada (FREC)
 //Ejemplo de llamada Oscilador(PUERTO,FREC);
 void Oscilador (unsigned char salida,unsigned int frecuencia)
 {
@@ -180,6 +179,7 @@ static const struct {const char letra, *codigo;} TablaMorse[] =
 };
 
 //Funcion de codificar
+//Descripcion: Codifica un texto en letras a morse (puntos y rayas)
 //Recibe: string de texto para codificar a CW
 //Retorna: string de texto codificado en CW
 //Ejemplo codificar("CQ CQ CQ ");
@@ -200,4 +200,16 @@ String codificar(const char *string)
     textomorse += "  "; //Agrego dos espacios de tiempo adicional para separar los caracteres
    }
   return textomorse;  
+}
+
+//Funcion destello
+//Descripcion: Hace destellar un led por 1 vez segundo.
+//Recibe: Nada.
+//Retorna: Nada.
+void destello(unsigned char led)
+{
+  digitalWrite(led,HIGH);
+  delay(200);
+  digitalWrite(led,LOW);
+  delay(800);
 }
